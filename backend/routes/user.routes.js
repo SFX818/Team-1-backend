5f8e0a9295874c9e18165302505974fb896c18a7
Gabbytam <gtam2@fordham.edu>
2021-01-02 21:08:54 -0800

    experiementing how to get around cors

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,7 +4,7 @@ const profileController = require('../controllers/profile.controller')
 
 module.exports = function(app) {
     app.use( (req,res, next) => {
-        // set header and allow use of x access token ( we will use this to pass our token )
+        //set header and allow use of x access token ( we will use this to pass our token )
         res.header(
             "Access-Control-Allow-Headers",
             "x-access-token, Origin, Content-type, Accept"
